41fc1d7dc543f687f93bc0f4c6db34170ebb1a15
Huynh <huynh156@example.com>
2026-07-20 23:48:08 +0700

    feat: build checkout form view & handle payment gateway callbacks

---
+++ Models/CheckOutVM.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace FashionHubWeb.Models
+{
+    public class CheckOutVM
+    {
+        public bool SameAddress { get; set; }
+        
+        [Required(ErrorMessage = "Full Name is required.")]
+        public string? FullName { get; set; }
+        
+        [Required(ErrorMessage = "Address is required.")]
+        public string? Address { get; set; }
+        
+        [Required(ErrorMessage = "Phone Number is required.")]
+        public int? PhoneNumber { get; set; }
+        
+        public string? Notes { get; set; }
+        
+        public string? CouponCode { get; set; }
+    }
+}
